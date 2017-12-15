39123f454d3d0c831ca03fa1e4ff26b39e6b57d0
Alex <oleksandr.barabanov@gmail.com>
2017-12-15 19:32:24 +0200

    fixed and got running app.component.spec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProvideFeedbackComponent } from './feedback/provide-feedback/provide-feedback.component';
 import { ReceivedFeedbackComponent } from './feedback/received-feedback/received-feedback.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
